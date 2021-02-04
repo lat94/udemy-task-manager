f3b6629e4309e792c6e03d0aba311d5c738d80a2
Lucas Alves Teixeira <lucas.alvestx94@gmail.com>
2021-02-04 17:55:23 -0300

    s10e77

--- mongodb.js
+++ mongodb.js
@@ -1,21 +1,66 @@
-const mongodb = require('mongodb');
-const MongoClient = mongodb.MongoClient;
+const { MongoClient, ObjectID } = require('mongodb');
+
+
 const connectionURL = 'mongodb://127.0.0.1:27017';
 const databaseName = 'task-manager';
 
+const id = new ObjectID();
+
 MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
-    console.log(error);
     if (error) {
-        return console.log('Unable to connect to database!');
+        return console.log('Unable to connect to database!', error);
     }
 
     console.log('Connected to database!');
 
     const db = client.db(databaseName);
 
-    db.collection('users').insertOne({
-        name: 'Lucas',
-        age: 26
-    });
+    /*db.collection('users').insertOne({
+        name: 'Viktor',
+        age: 54
+    }, (error, result) => {
+        if (error) {
+            return console.log('Unable to insert user');            
+        }
+
+        console.log(result.ops);
+    });*/
+
+    /*db.collection('users').insertMany([
+        {
+            name: 'Lucas2',
+            age: 33
+        },
+        {
+            name: 'Albert',
+            age: 33
+        }
+    ], (error, result) => {
+        if (error) {
+            return console.log('Unable to insert documents');
+        }
+
+        console.log(result.ops);
+    });*/
+
+    /*db.collection('tasks').insertMany([
+        {
+            description: 'First task',
+            completed: true
+        },
+        {
+            description: 'Second task',
+            completed: false
+        }
+
+    ], (error, result) => {
+        if (error) {
+            return console.log('Unable to insert tasks');
+        }
+
+        console.log(result.ops);
+    });*/
+
+
 
 });
