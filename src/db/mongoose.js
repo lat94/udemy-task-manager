4d6d50b5490e66a9a98d6c505df732672e7bb194
Lucas Alves Teixeira <lucas.alvestx94@gmail.com>
2021-02-11 20:09:05 -0300

    s11e89

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -6,44 +6,6 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useCreateIndex: true
 });
 
-const User = mongoose.model('User', {
-    name: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    age: {
-        type: Number,
-        min: 0,
-        max: 150,
-        default: 0
-    },
-    email: {
-        type: String,
-        required: true,
-        lowercase: true,
-        trim: true,
-        validate(value) {
-            if (!validator.isEmail(value)) {
-                throw new Error('Email is invalid');
-
-            }
-        }
-    },
-    password: {
-        type: String,
-        required: true,
-        minlength: 7,
-        trim: true,
-        validate(value) {
-            if (value.toLowerCase().includes('password')) {
-                throw new Error('Password cannot contain "password"');
-            }
-        }
-    }
-
-});
-
 const Task = mongoose.model('Task', {
     description: {
         type: String,
@@ -55,20 +17,3 @@ const Task = mongoose.model('Task', {
         default: false
     }
 });
-
-
-const user = new User({
-    name: 'Ruth',
-    age: 23,
-    email: 'ruth-green@hotmail.com',
-    password: 'sdsd'
-});
-
-
-const task = new Task({
-    description: 'Finish complete NodeJS udemy course',
-})
-
-task.save()
-    .then(result => console.log(result))
-    .catch(error => console.log(error))
---
+++ src/models/user.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const User = mongoose.model('User', {
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    age: {
+        type: Number,
+        min: 0,
+        max: 150,
+        default: 0
+    },
+    email: {
+        type: String,
+        required: true,
+        lowercase: true,
+        trim: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error('Email is invalid');
+
+            }
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 7,
+        trim: true,
+        validate(value) {
+            if (value.toLowerCase().includes('password')) {
+                throw new Error('Password cannot contain "password"');
+            }
+        }
+    }
+
+});
+
+module.exports = User;
