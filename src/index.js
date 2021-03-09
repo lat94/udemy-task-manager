25888453661daf7c88f77849d5697a575e642a30
Lucas Alves Teixeira <lucas.alvestx94@gmail.com>
2021-03-09 19:52:36 -0300

    s11e98

--- src/index.js
+++ src/index.js
@@ -9,65 +9,112 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json())
 
 
-app.post('/users', (req, res) => {
+app.post('/users', async (req, res) => {
     const user = new User(req.body);
-    user.save().then((result) => {
-        res.status(201).send(result);
-    }).catch((error) => {
+
+    try {
+        await user.save();
+        res.status(201).send(user);
+    } catch (error) {
+        console.error(error);
         res.status(400).send(error);
-    });
+    }
 });
 
-app.get('/users', (req, res) => {
-    User.find({}).then((users) => {
+app.get('/users', async (req, res) => {
+
+    try {
+        const users = await User.find({});
         res.send(users);
-    }).catch((error) => {
+    } catch (error) {
         res.status(500).send({ status: 500, message: error });
-    });
+    }
 });
 
-app.get('/users/:id', (req, res) => {
+app.get('/users/:id', async (req, res) => {
     const _id = req.params.id;
-    User.findById({ _id }).then((user) => {
+
+    try {
+        const user = await User.findById({ _id });
         if (!user) {
-            return res.status(404).send;
+            return res.status(404).send();
         }
-
         res.send(user);
-    }).catch((error) => {
+    } catch (error) {
         res.status(500).send({ status: 500, message: error });
-    });
+    }
 });
 
-app.post('/tasks', (req, res) => {
+app.patch('/users/:id', async (req, res) => {
+    const userToBeUpdated = req.body;
+    const updated = Object.keys(userToBeUpdated)
+    const allowedUpdates = ['name', 'email', 'password', 'age'];
+    const isValidOperation = updated.every((update) => allowedUpdates.includes(update));
+    const id = req.params.id;
+
+    if (!isValidOperation) {
+        return res.status(400).send({ error: 'Invalid updates' })
+    }
+
+    try {
+        const userUpdated = await User.findByIdAndUpdate(id, userToBeUpdated, { new: true, runValidators: true });
+
+        if (!userUpdated) {
+            return res.status(404).send();
+        } 
+
+        res.send(userUpdated)
+
+    } catch (error) {
+        res.status(400).send();
+    }
+});
+
+app.post('/tasks', async (req, res) => {
     const task = new Task(req.body);
-    task.save().then((result) => {
-        res.status(201).send(result);
-    }).catch((error) => {
+
+    try {
+        await task.save();
+        res.status(201).send(task);
+    } catch (error) {
         res.status(400).send(error);
-    });
+    }
 });
 
-app.get('/tasks', (req, res) => {
-    Task.find({}).then((tasks) => {
+app.get('/tasks', async (req, res) => {
+    try {
+        const tasks = await Task.find({})
         res.send(tasks);
-    }).catch((error) => {
+
+    } catch (error) {
         res.status(500).send({ status: 500, message: error });
-    });
+    }
 });
 
-app.get('/tasks/:id', (req, res) => {
+app.get('/tasks/:id', async (req, res) => {
     const _id = req.params.id;
-    Task.findById({ _id }).then((task) => {
-        if (!task) {
-            return res.status(404).send;
-        }
 
+    try {
+        const task = await Task.findById({ _id });
+        if (!task) {
+            return res.status(404).send();
+        }
         res.send(task);
-    }).catch((error) => {
+    } catch (error) {
         res.status(500).send({ status: 500, message: error });
-    });
+    }
 });
+
+app.patch('/tasks/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+        const taskToBeUpdated = req.body;
+
+        Task.findByIdAndUpdate(id, taskToBeUpdated, { new: true, runValidators: true })
+    } catch (error) {
+        
+    }
+})
 
 app.listen(PORT, () => {
     console.log(`Server is running on port: ${PORT}`);
