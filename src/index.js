b6534ed87be782eda7899424ba3321aa031f30e3
Lucas Alves Teixeira <lucas.alvestx94@gmail.com>
2021-03-11 17:24:11 -0300

    s12e103

--- src/index.js
+++ src/index.js
@@ -1,13 +1,35 @@
 const express = require('express');
-const userRouter = require('./routers/user');
-const taskRouter = require('./routers/task');
+const bcrypt = require('bcryptjs');
+const routes = require('./routers/index');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use([userRouter, taskRouter]);
+app.use(routes);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port: ${PORT}`);
-})
+});
+
+
+const myFunction = async () => {
+    const password = "luC@S";
+    const hashedPassword = await bcrypt.hash(password, 8);
+
+    console.log('------password------');
+    console.log(password);
+    console.log('------------');
+
+    console.log('------hashedPassword------');
+    console.log(hashedPassword);
+    console.log('------------');
+
+    const isMatch = await bcrypt.compare(password, hashedPassword);
+
+    console.log('------unhashedPassword------');
+    console.log(isMatch);
+    console.log('------------');
+}
+
+myFunction();
---
+++ src/routers/index.js
@@ -0,0 +1,4 @@
+const taskRoutes = require('./task');
+const userRoutes = require('./user');
+
+module.exports = [taskRoutes, userRoutes]
