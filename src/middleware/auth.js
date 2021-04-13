efe334c831cc2e675ca77780372926a08c676f61
Lucas Alves Teixeira <lucas.alvestx94@gmail.com>
2021-04-13 18:31:56 -0300

    s16e138

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -3,7 +3,6 @@ const User = require('../models/user');
 
 const { JWT_SECRET } = process.env;
 
-
 const auth = async (req, res, next) => {
     try {
         const token = req.header('Authorization').replace('Bearer ', '');
